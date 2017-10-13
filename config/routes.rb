aa9c05741b1507335263b23113d7efce2f7e970f
Tokinori Matsushima <m-toki@nttpc.co.jp>
2017-10-13 16:19:17 +0000

    dive14-2

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :blogs, only: [:index]
-  resources :poems, only: [:index]
+  resources :poems, only: [:index, :show]
 
   # Example resource route with options:
   #   resources :products do
